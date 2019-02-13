235b457f3bf164962070c8e89e64041b66b8bb41
Seamile <lanhuermao@gmail.com>
2019-02-13 15:33:18 +0800

    添加了 Profile 模型

--- user/models.py
+++ user/models.py
@@ -8,6 +8,7 @@ class User(models.Model):
         ('male', '男性'),
         ('female', '女性'),
     )
+
     LOCATION = (
         ('bj', '北京'),
         ('sh', '上海'),
@@ -17,6 +18,7 @@ class User(models.Model):
         ('xa', '西安'),
         ('wh', '武汉'),
     )
+
     phonenum = models.CharField(max_length=16, unique=True, verbose_name='手机号')
     nickname = models.CharField(max_length=32, unique=True, verbose_name='昵称')
     sex = models.CharField(max_length=8, choices=SEX, verbose_name='性别')
@@ -33,6 +35,13 @@ class User(models.Model):
         birthday = datetime.date(self.birth_year, self.birth_month, self.birth_day)
         return (today - birthday).days // 365
 
+    @property
+    def profile(self):
+        '''用户个人资料'''
+        if not hasattr(self, '_profile'):
+            self._profile, _ = Profile.objects.get_or_create(id=self.id)
+        return self._profile
+
     def to_dict(self):
         return {
             'phonenum': self.phonenum,
@@ -42,3 +51,34 @@ class User(models.Model):
             'avatar': self.avatar,
             'location': self.location,
         }
+
+
+class Profile(models.Model):
+    '''个人资料'''
+    SEX = (
+        ('male', '男性'),
+        ('female', '女性'),
+    )
+
+    LOCATION = (
+        ('bj', '北京'),
+        ('sh', '上海'),
+        ('gz', '广州'),
+        ('sz', '深圳'),
+        ('cd', '成都'),
+        ('xa', '西安'),
+        ('wh', '武汉'),
+    )
+
+    dating_sex = models.CharField(max_length=8, choices=SEX, verbose_name='匹配的性别')
+    location = models.CharField(max_length=16, choices=LOCATION, verbose_name='目标城市')
+
+    min_distance = models.IntegerField(default=1, verbose_name='最小查找范围')
+    max_distance = models.IntegerField(default=10, verbose_name='最大查找范围')
+
+    min_dating_age = models.IntegerField(default=18, verbose_name='最小交友年龄')
+    max_dating_age = models.IntegerField(default=50, verbose_name='最大交友年龄')
+
+    vibration = models.BooleanField(default=True, verbose_name='开启震动')
+    only_matche = models.BooleanField(default=True, verbose_name='不让为匹配的人看我的相册')
+    auto_play = models.BooleanField(default=True, verbose_name='自动播放视频')
