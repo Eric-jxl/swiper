1f770b85b053c40cd88375b1600f4496c05f9343
Seamile <lanhuermao@gmail.com>
2019-02-14 15:33:57 +0800

    添加了初始化脚本

---
+++ social/api.py
@@ -0,0 +1,26 @@
+from libs.http import render_json
+
+
+def rcmd_users(request):
+    '''获取推荐列表'''
+    pass
+
+
+def like(request):
+    return render_json()
+
+
+def superlike(request):
+    return render_json()
+
+
+def dislike(request):
+    return render_json()
+
+
+def rewind(request):
+    return render_json()
+
+
+def show_liked_me(request):
+    return render_json()
