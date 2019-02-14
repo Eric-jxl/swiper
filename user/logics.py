ead4db93b815122567a1e1850e8d17d3c3d3ffbe
Seamile <lanhuermao@gmail.com>
2019-02-14 11:43:54 +0800

    添加了 celery 异步操作

--- user/logics.py
+++ user/logics.py
@@ -1,6 +1,7 @@
 import re
 import os
 from random import randrange
+from urllib.parse import urljoin
 
 import requests
 from django.core.cache import cache
@@ -8,6 +9,8 @@ from django.conf import settings
 
 from swiper import config
 from common import keys
+from worker import celery_app
+from libs.qncloud import upload_qncloud
 
 
 def is_phonenum(phonenum):
@@ -49,3 +52,18 @@ def save_upload_file(filename, upload_file):
         for chunk in upload_file.chunks():
             newfile.write(chunk)
     return filename, filepath
+
+
+@celery_app.task
+def save_avatar(user, avatar):
+    '''保存用户形象'''
+    # 将文件保存到本地
+    filename = 'Avatar-%s' % user.id
+    filename, filepath = save_upload_file(filename, avatar)
+
+    # 将本地文件上传到七牛云
+    upload_qncloud(filename, filepath)
+
+    # 记录头像 URL
+    user.avatar = urljoin(config.QN_HOST, filename)
+    user.save()
